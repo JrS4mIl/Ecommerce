7e49c962eb5b60e143fbc2f9cd45f7dfd8837ee2
Yesil <yesil@gmail.com>
2024-01-23 18:26:28 +0300

    feat: cart model

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
-    path('store/',include('store.urls'))
+    path('store/',include('store.urls')),
+    path('cart/',include('carts.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
