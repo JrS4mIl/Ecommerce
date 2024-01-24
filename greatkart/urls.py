81a6a4124ac915a7f1e69bc0bb96f307c0b1aa43
Yesil <yesil@gmail.com>
2024-01-24 16:32:14 +0300

    feat: cart views

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
     path('store/',include('store.urls')),
-    path('cart/',include('carts.urls'))
+    path('cart/',include('carts.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
