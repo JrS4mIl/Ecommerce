d1a87d24723ab4a717fd221fc8dadd12f53a13de
Yesil <yesil@gmail.com>
2024-01-30 17:15:41 +0300

    feat: Forgot Password with Secure Validation Links

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,11 @@ urlpatterns=[
     path('register/',register,name='register'),
     path('login/',login,name='login'),
     path('logout/',logout,name='logout'),
-    path('activate/<uidb64>/<token>/',activate,name='activate')
+    path('activate/<uidb64>/<token>/',activate,name='activate'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('forgotPassword/', forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', resetPassword, name='resetPassword'),
 
 
 ]
